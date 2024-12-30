f8619ca6e074d8824bde5d396e05fa906685310d
quanna19596 <quanna.195.96@gmail.com>
2024-12-30 22:11:46 +0700

    https://threejs-journey.com/lessons/haunted-house 1:18:00

--- src/chapter2-3.js
+++ src/chapter2-3.js
@@ -19,13 +19,30 @@ const scene = new THREE.Scene()
 scene.add(axesHelper);
 
 /**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader();
+
+// Floor
+const floorAlphaTexture = textureLoader.load('./floor/alpha.jpg');
+const floorColorTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_diff_1k.jpg');
+const floorARMTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_arm_1k.jpg');
+const floorNormalTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_nor_gl_1k.jpg');
+const floorDisplacementTexture = textureLoader.load('./floor/coast_sand_rocks_02_1k/coast_sand_rocks_02_disp_1k.jpg');
+
+
+/**
  * House
  */
 
 // Floor
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
-  new THREE.MeshStandardMaterial()
+  new THREE.MeshStandardMaterial({
+    alphaMap: floorAlphaTexture,
+    transparent: true,
+    map: floorColorTexture
+  })
 )
 floor.rotation.x = -Math.PI / 2;
 scene.add(floor);
